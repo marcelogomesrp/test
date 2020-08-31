0932f6d448eda1c88b418c3fe47fab9133923575
Marcelo Gomes de Paula <marcelogomesrp@gmail.com>
2020-08-31 19:08:27 -0300

    first

---
+++ src/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo > ./bin/index.html
+echo '<!DOCTYPE html>' >> ./bin/index.html
+echo '<html lang="en">' >> ./bin/index.html
+echo '<head>' >> ./bin/index.html
+echo '    <meta charset="UTF-8">' >> ./bin/index.html
+echo '    <meta name="viewport" content="width=device-width, initial-scale=1.0">' >> ./bin/index.html
+echo '    <title>Test page</title>' >> ./bin/index.html
+echo '</head>' >> ./bin/index.html
+echo '<body>' >> ./bin/index.html
+echo '    <h1>Just a test</h1>' >> ./bin/index.html
+echo '</body>' >> ./bin/index.html
+echo '</html>' >> ./bin/index.html
